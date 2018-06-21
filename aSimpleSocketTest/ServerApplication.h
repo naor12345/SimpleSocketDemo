8416156d63d884c53084a2390c24b9d55189eed9
naor12345 <naor12345@live.com>
2018-06-21 23:05:30 +0800

    rewrite the server code.

---
+++ aSimpleSocketTest/ServerApplication.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#include <netinet/in.h> //for sockaddr
+#include <string.h> //for memset
+#include <arpa/inet.h> //for inet_ntoa
+#include <unistd.h> //for fork
+#include <stdlib.h> // for exit
+
+#define SERVERPORT 53556
+#define BACKLOG 10
+#define MAXSIZE 1024
+    
+
+void str_echo(int sockfd);
---
+++ aSimpleSocketTest/ServerBook.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+#include <netinet/in.h> //for sockaddr
+#include <string.h> //for memset
+#include <arpa/inet.h> //for inet_ntoa
+#include <unistd.h> //for fork
+#include <stdlib.h> // for exit
+
+#include "aSimpleSocketTest/ServerApplication.h"
+
+int main()
+{  
+    int socketfd, connfd;
+    pid_t childpid;
+    socklen_t clilen;
+    sockaddr_in cliaddr, servaddr;
+
+    // create socket
+    if((socketfd = socket(AF_INET, SOCK_STREAM, 0))==-1)
+    {
+        cerr<<"socket fail"<<endl;
+        exit(1);
+    }
+
+    cout<<"socket: "<<socketfd<<endl;
+
+    // set server addr
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERVERPORT);
+    // bind
+    if(bind(socketfd, (sockaddr*)(&servaddr), sizeof(sockaddr)) == -1)
+    {
+        cerr<<"bind error"<<endl;
+        exit(1);
+    }
+    // listen
+    if(listen(socketfd, BACKLOG) == -1)
+    {
+        cerr<<"listen error"<<endl;
+        exit(1);
+    }
+
+    while(true)
+    {
+        clilen = sizeof(cliaddr);
+        if((connfd = accept(socketfd, (sockaddr*)(&cliaddr), &clilen)) < 0)
+        {
+            cerr<<"accept error"<<endl;
+            continue;
+        }
+
+        if((childpid = fork()) == 0)
+        {
+            close(socketfd);
+            // application function
+            str_echo(connfd);
+            exit(0);
+        }
+        close(connfd);
+    }
+}
